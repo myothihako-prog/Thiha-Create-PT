949c6fbde3c9edbea62c44aeab29c459eaf49b16
myothihako-prog <myothiha.ko@crpusd.org>
2026-03-05 09:37:47 -0800

    Create Main.py for subject quiz selection
    Initialize a quiz program with subject selection.

---
+++ Main.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+
+#List for Algebra 1 and Spanish 1
+#If user chooses Algebra 1 then assign Algebra 1 to subject
+#Else choose Spanish 1 then assign Spanish 1 to subject
+
+
+subject = input("Do you want to have to quiz of Spanish 1 or Algebra 1? Say S or A")
+
+
+def user_chosen_subject(subject):
+    while 1+1 == 2:
+       
+     if subject == "A":
+       
+     elif subject == "S":
+       
+     else:
+         print("Try Again")
+   
+
+
+
